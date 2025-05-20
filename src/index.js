42d82ae21163806858ab29516c5cd4d5cb6de18f
Katya Weicht <katya.weicht@gmail.com>
2025-05-19 18:43:53 -0600

    Write functionality to add tasks to list upon submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  let taskList = document.querySelector("#tasks")
+
+  form.addEventListener("submit", () => {
+    const userInput = event.target.elements["new-task-description"].value
+    buildToDo(userInput)
+    
+  })
+
+  function buildToDo(userInput) {
+    event.preventDefault()
+    let list = document.createElement("li")
+    list.textContent = userInput
+    taskList.append(list)
+  }
 });
+
